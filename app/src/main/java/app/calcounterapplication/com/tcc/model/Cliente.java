7ff99adba95186a6ffc274d1306ccb517efb9559
igor17400 <igorlima1740@gmail.com>
2018-10-17 19:42:18 -0300

    Bora ver

--- app/src/main/java/app/calcounterapplication/com/tcc/model/Cliente.java
+++ app/src/main/java/app/calcounterapplication/com/tcc/model/Cliente.java
@@ -1,5 +1,27 @@
 package app.calcounterapplication.com.tcc.model;
 
-public class Cliente extends Usuario {
+import com.google.firebase.database.DatabaseReference;
+
+import app.calcounterapplication.com.tcc.config.ConfigFirebase;
+
+public class Cliente extends PessoaFisica {
+    private String cartaoDeCredido;
+
+    public String getCartaoDeCredido() {
+        return cartaoDeCredido;
+    }
+
+    public void setCartaoDeCredido(String cartaoDeCredido) {
+        cartaoDeCredido = cartaoDeCredido;
+    }
+
+    public void salvar() {
+        //super.salvar();
+        DatabaseReference firebaseRef = ConfigFirebase.getFirebaseDatabase();
+        DatabaseReference usuarios = firebaseRef.child("usuarios").child("clientes")
+                .child(getId());
+        usuarios.setValue(this);
+    }
 
 }
+
---
+++ app/src/main/java/app/calcounterapplication/com/tcc/model/PessoaFisica.java
@@ -0,0 +1,23 @@
+package app.calcounterapplication.com.tcc.model;
+
+public class PessoaFisica extends Usuario {
+
+    private String rg;
+    private String cpf;
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+}
