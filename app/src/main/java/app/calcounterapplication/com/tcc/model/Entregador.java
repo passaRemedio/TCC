171bc597e63494cb0657d6b2ac36932307bcdfe7
igor17400 <igorlima1740@gmail.com>
2018-10-17 19:44:28 -0300

    Bora ver

---
+++ app/src/main/java/app/calcounterapplication/com/tcc/model/Entregador.java
@@ -0,0 +1,28 @@
+package app.calcounterapplication.com.tcc.model;
+
+import com.google.firebase.database.DatabaseReference;
+
+import app.calcounterapplication.com.tcc.config.ConfigFirebase;
+
+public class Entregador extends PessoaFisica{
+
+    private String contaBancaria;
+
+    public String getContaBancaria() {
+        return contaBancaria;
+    }
+
+    public void setContaBancaria(String contaBancaria) {
+        this.contaBancaria = contaBancaria;
+    }
+
+    public void salvar() {
+        //super.salvar();
+        DatabaseReference firebaseRef = ConfigFirebase.getFirebaseDatabase();
+        DatabaseReference usuarios = firebaseRef.child("usuarios").child("entregadores")
+                .child(getId());
+        usuarios.setValue(this);
+    }
+
+}
+
